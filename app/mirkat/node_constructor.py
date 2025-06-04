05aad4b4f059c63f22b609201ccee2e9bf9b3f70
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2025-06-04 09:34:55 +0100

    Remove the plot node form the constructor to avoid confusion

--- app/mirkat/node_constructor.py
+++ app/mirkat/node_constructor.py
@@ -309,59 +309,6 @@ class PlotNode(node):
         self.client = genai.Client(api_key=GOOGLE_API_KEY)
         self.set_model()
 
-    def set_model(self):
-        config_with_code = types.GenerateContentConfig(
-            tools=[types.Tool(code_execution=types.ToolCodeExecution())],
-            temperature=0.0,
-            )
-        self.model = self.client.chats.create(model=self.llm, config=config_with_code)
-
-    def run_model(self, messages):
-        """Run the model with the given messages."""
-        #print(f"--- Message going to the llm_master: {messages}---")
-        logging.info(f"Message going to the llm_master: {messages}")
-        response_plot = self.model.send_message(messages)
-        return response_plot
-    
-    def handle_response(self, response_plot):
-        plotting_tools_instance = PlotFunctons('', response_plot)
-        plotting_tools_instance.handle_response()
-
-    def get_node(self, state):
-        messages = state['request']
-        logging.info(f"Messages received in PlotNode: {messages}")
-        queries = SQL_QUERIES # state['table']
-
-        response_plot = self.run_model(str(queries) + self.instructions + messages.content)
-        
-        plotting_tools_instance = PlotFunctons('', response_plot)        
-        plot = plotting_tools_instance.handle_response()
-        
-        answer = ''
-        for part in response_plot.candidates[0].content.parts:
-            if part.text is not None:
-                answer = answer + f"{part.text}\n"
-        answer_b = answer
-        if plot:
-            buf = io.BytesIO()
-            if not isinstance(plot, Chart):
-                plot.savefig(buf, format='png') # Or another format like 'jpeg'
-            elif isinstance(plot, Chart):
-                plot.save(buf, format='json')
-            buf.seek(0)
-            image_base64 = base64.b64encode(buf.read()).decode('utf-8')
-            buf.close()
-            # response_plot.candidates[0].content.parts[0].text =  f"binary_image: {image_base64}"
-            answer_b = answer + f"binary_image: {image_base64}"
-        history = state.get("history", [])
-        return {**state,
-                "messages": AIMessage(content=""),
-                "answer": answer,
-                "request": AIMessage(content=answer_b),
-                "answer_source": 'PlotNode',
-                "history": history + [messages], # Update history with the new message
-            }
-    
     
 
 class LiteratureNode(node):
